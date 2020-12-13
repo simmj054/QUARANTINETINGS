88945b954d976503373847271b4f86710ecb726c
simmj054 <haiijadaz@gmail.com>
2020-12-12 23:30:42 -0500

    For this, i added all the reast of my assets and also put hover scripts on the buttons. Also added a character with character movement script

---
+++ Assets/closethover.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class closethover : MonoBehaviour
+{
+    public GameObject closet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        closet.SetActive(false);
+    }
+public void OnMouseOver()
+    {
+
+        closet.SetActive(true);
+    }
+
+    public void OnMouseExit()
+    { 
+        closet.SetActive(false);
+    }
+}
---
+++ Assets/playermovement.cs
@@ -0,0 +1,44 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+
+public class playermovement : MonoBehaviour
+{
+    public float speed;
+    private Rigidbody2D myRigidBody;
+    private Vector3 change;
+    private SpriteRenderer sr;
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        change = Vector3.zero;
+        change.x = Input.GetAxisRaw("Horizontal");
+        change.y = Input.GetAxisRaw("Vertical");
+        if(change != Vector3.zero)
+        {
+            MoveCharacter();
+        }
+        Debug.Log(change);
+
+        float horizontalInput = Input.GetAxis("Horizontal");
+        if (horizontalInput > 0)
+        {
+            sr.flipX = true;
+        }
+        else
+        {
+            sr.flipX = false;
+        }
+    }
+    void MoveCharacter()
+    {
+        myRigidBody.MovePosition(
+            transform.position + change * speed * Time.deltaTime);
+    }
+}
